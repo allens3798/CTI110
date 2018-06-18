bc4e4c0d2520075c7d31db8b934f5142340d92e5
allens3798 <40345701+allens3798@users.noreply.github.com>
2018-06-17 20:33:50 -0400

    Add files via upload

---
+++ P2T1B.py
@@ -0,0 +1,27 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/Pat/Desktop/CTI110/P2T1.py ================
+Enter projected Sales 1000
+The profit is $ 230.0
+>>> 
+>>> 
+================ RESTART: C:/Users/Pat/Desktop/CTI110/P2T1.py ================
+Enter projected Sales 1000
+Traceback (most recent call last):
+  File "C:/Users/Pat/Desktop/CTI110/P2T1.py", line 10, in <module>
+    print ('The profit is $', format (profit, ',.2f'))
+NameError: name 'profit' is not defined
+>>> 
+================ RESTART: C:/Users/Pat/Desktop/CTI110/P2T1.py ================
+Enter projected Sales 1000
+The profit is $ 230.00
+>>> 
+================ RESTART: C:/Users/Pat/Desktop/CTI110/P2T1.py ================
+Enter projected Sales 10000
+The profit is $ 2,300.00
+>>> 
+================ RESTART: C:/Users/Pat/Desktop/CTI110/P2T1.py ================
+Enter projected Sales 750930
+The profit is $ 172,713.90
+>>> 
